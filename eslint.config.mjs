a497e190733136c128f1dafa05c27b13f662ce89
sohelmohammed0 <sohelmohammed88004329@gmail.com>
2026-03-10 09:41:24 +0530

    Configure ESLint for Node globals

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,4 +2,12 @@ import js from "@eslint/js";
 
 export default [
   js.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        module: "readonly",
+        require: "readonly",
+      },
+    },
+  },
 ];

72d374316bdaf6d6ad65374892f48153f42a74b4
sohelmohammed0 <sohelmohammed88004329@gmail.com>
2026-03-10 09:43:35 +0530

    Add Jest globals to ESLint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,11 +2,24 @@ import js from "@eslint/js";
 
 export default [
   js.configs.recommended,
+
+  // Node environment
   {
     languageOptions: {
       globals: {
         module: "readonly",
         require: "readonly",
+      },
+    },
+  },
+
+  // Jest environment (only for test files)
+  {
+    files: ["**/*.test.js"],
+    languageOptions: {
+      globals: {
+        test: "readonly",
+        expect: "readonly",
       },
     },
   },
